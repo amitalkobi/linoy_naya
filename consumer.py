3878ae5ce377da22004fae7d0e66f861880fa3fd
amitalkobi <amit.alkobi14@gmail.com>
2022-09-07 20:44:26 +0300

    Update consumer.py
    nice but not working

--- consumer.py
+++ consumer.py
@@ -1,120 +1,21 @@
-from kafka import KafkaConsumer
-import json
-import os
-import pyspark.sql
-from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.sql import DataFrameWriter
-import mysql.connector as mc
-# import mysql_conf as myconf
-from pyspark.sql.types import StringType, StructType, IntegerType, FloatType, DateType,StructField,LongType
-from mysql.connector import errorcode
-from time import sleep
-from datetime import datetime
+from pyspark.sql.types import StringType, StructType, IntegerType, FloatType
 from pyspark.sql import SparkSession
 import os
-import pyarrow as pa
-import json as js
-import pyspark.sql.functions as f
-from pyspark.sql import types as t
-from IPython.core.display import display
-from dateutil.parser import parse
-import pandas as pd
 import mysql.connector as mc
 
-# connector to mysql
+os.environ[
+    'PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark_session:spark_session-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
 
-mysql_config_user = 'naya'
-mysql_config_password = 'NayaPass1!'
-mysql_config_host = 'localhost'
-mysql_config_port = '3306'
 
-mysql_conn = mc.connect(
-    user=mysql_config_user,
-    password=mysql_config_password,
-    host=mysql_config_host,
-    port=mysql_config_port,
-    autocommit=True,  # <--
-    # database=mysql_database_name)
-)
-
-mysql_create_tbl_events = '''create table if not exists yad2.yad04
-    (current_ts varchar (78) primary key ,
-    record_id numeric NULL,
-    ad_number numeric NULL,
-    price varchar (20) NULL, 
-    currency varchar (10) NULL, 
-    city_code numeric NULL, 
-    city varchar (30) NULL, 
-    street varchar (78) NULL, 
-    AssetClassificationID_text varchar (200) NULL, 
-    coordinates varchar (200) NULL, 
-    ad_date varchar (20) NULL, 
-    date_added varchar (20) NULL, 
-    no_of_rooms numeric NULL, 
-    floor_no numeric NULL, 
-    size_in_sm numeric NULL,
-    price_per_SM numeric NULL);'''
-
-mysql_create_tbl_df = '''create table if not exists yad2.df
-    (city_code numeric primary key ,
-    avg_SquareMeter numeric,
-    avg_price_per_SM numeric ,
-    count_city numeric);'''
-
-mysql_cursor = mysql_conn.cursor()
-mysql_cursor.execute(mysql_create_tbl_events)
-mysql_cursor.execute(mysql_create_tbl_df)
-mysql_cursor.close()
-
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
-
-bootstrapServers = '34.71.172.85:9092'
-topics = "yad2"
-
-spark = SparkSession\
-        .builder\
-        .appName("yad2")\
-        .getOrCreate()
-
-schema = StructType() \
-    .add('current_ts', StringType()) \
-    .add("record_id", IntegerType()) \
-    .add("ad_number", IntegerType()) \
-    .add("rooms", FloatType()) \
-    .add("floor", FloatType()) \
-    .add("SquareMeter", FloatType()) \
-    .add("price", FloatType()) \
-    .add("currency", StringType()) \
-    .add("city_code", IntegerType()) \
-    .add("city", StringType()) \
-    .add("street", StringType()) \
-    .add("AssetClassificationID_text", StringType()) \
-    .add("coordinates", StringType()) \
-    .add("date", StringType()) \
-    .add("date_added", StringType())
-
-df_kafka = spark\
-    .readStream\
-    .format("kafka")\
-    .option("kafka.bootstrap.servers", bootstrapServers)\
-    .option("subscribe", topics) \
-    .load()
-
-df_kafka = df_kafka.select(col("value").cast("string"))\
-    .select(from_json(col("value"), schema).alias("value"))\
-    .select("value.*")
-
-df_kafka.printSchema()
-
-df_kafka = df_kafka.withColumn("price_per_SM", df_kafka.price/df_kafka.SquareMeter)
-df_kafka = df_kafka.withColumn("current_ts", current_timestamp().cast('string'))
-
-df_CityAvgPrice = df_kafka\
-    .groupby("city_code")\
-    .agg(avg("SquareMeter").alias("avg_SquareMeter"),avg("price_per_SM").alias("avg_price_per_SM"),count("record_id").alias("count_city"))
-
-df_CityAvgPrice = df_CityAvgPrice.withColumn("current_ts", current_timestamp().cast('string'))
+def get_mysql_connection():
+    return mc.connect(
+        user='naya',
+        password='NayaPass1!',
+        host='localhost',
+        port='3306',
+        autocommit=True
+    )
 
 
 class InvalidRecordId(Exception):
@@ -132,14 +33,7 @@ def _validate_process_row_event(events):
 
 def procss_row(event):
     # connector to mysql
-    mysql_conn = mc.connect(
-        user=mysql_config_user,
-        password=mysql_config_password,
-        host=mysql_config_host,
-        port=mysql_config_port,
-        autocommit=True,  # <--
-        # database=mysql_database_name)
-    )
+    mysql_conn = get_mysql_connection()
 
     print(event)
     try:
@@ -148,14 +42,17 @@ def procss_row(event):
         print(f'Validation error: {e}')
         return
 
-
     insert_statement = """
     INSERT INTO yad2.yad04(current_ts, record_id, ad_number, price, currency, city_code, city, street, AssetClassificationID_text, coordinates, ad_date, date_added, no_of_rooms, floor_no, size_in_sm, price_per_SM)
         VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}',
                 '{}', '{}', '{}', '{}','{}','{}'); """
 
     mysql_cursor = mysql_conn.cursor()
-    sql = insert_statement.format(event["current_ts"], event["record_id"], event["ad_number"], event["price"], event["currency"], event["city_code"], event["city"], event["street"], event["AssetClassificationID_text"], event["coordinates"], event["date"], event["date_added"], event["rooms"], event["floor"], event["SquareMeter"], event["price_per_SM"])
+    sql = insert_statement.format(event["current_ts"], event["record_id"], event["ad_number"], event["price"],
+                                  event["currency"], event["city_code"], event["city"], event["street"],
+                                  event["AssetClassificationID_text"], event["coordinates"], event["date"],
+                                  event["date_added"], event["rooms"], event["floor"], event["SquareMeter"],
+                                  event["price_per_SM"])
     mysql_conn.commit()
     mysql_cursor.execute(sql)
     mysql_cursor.close()
@@ -178,21 +75,13 @@ def _validate_process_df_event(event):
 
 def procss_df(events):
     # connector to mysql
-    mysql_conn = mc.connect(
-        user=mysql_config_user,
-        password=mysql_config_password,
-        host=mysql_config_host,
-        port=mysql_config_port,
-        autocommit=True,  # <--
-        # database=mysql_database_name)
-    )
+    mysql_conn = get_mysql_connection()
 
     try:
         _validate_process_df_event(events)
     except Exception as e:
         print(f'Validation error: {e}')
         return
-
 
     insert_statement = """
     INSERT INTO yad2.df(city_code, avg_SquareMeter, avg_price_per_SM, count_city)
@@ -202,9 +91,10 @@ def procss_df(events):
         """
 
     mysql_cursor = mysql_conn.cursor()
-    sql_update = update_statement.format(events["avg_SquareMeter"], events["avg_price_per_SM"], events["count_city"], events["city_code"])
-    sql_insert = insert_statement.format(events["city_code"], events["avg_SquareMeter"], events["avg_price_per_SM"], events["count_city"])
-    # print(sql)
+    sql_update = update_statement.format(events["avg_SquareMeter"], events["avg_price_per_SM"], events["count_city"],
+                                         events["city_code"])
+    sql_insert = insert_statement.format(events["city_code"], events["avg_SquareMeter"], events["avg_price_per_SM"],
+                                         events["count_city"])
     mysql_conn.commit()
     try:
         mysql_cursor.execute(sql_insert)
@@ -216,34 +106,113 @@ def procss_df(events):
         print('update df')
 
 
+def _init_mysql():
+    mysql_conn = get_mysql_connection()
 
-Insert_To_MYSQL_DB=df_kafka\
-    .writeStream\
-    .foreach(procss_row)\
-    .outputMode("append") \
-    .start()
+    mysql_create_tbl_events = '''create table if not exists yad2.yad04
+        (current_ts varchar (78) primary key ,
+        record_id numeric NULL,
+        ad_number numeric NULL,
+        price varchar (20) NULL, 
+        currency varchar (10) NULL, 
+        city_code numeric NULL, 
+        city varchar (30) NULL, 
+        street varchar (78) NULL, 
+        AssetClassificationID_text varchar (200) NULL, 
+        coordinates varchar (200) NULL, 
+        ad_date varchar (20) NULL, 
+        date_added varchar (20) NULL, 
+        no_of_rooms numeric NULL, 
+        floor_no numeric NULL, 
+        size_in_sm numeric NULL,
+        price_per_SM numeric NULL);
+        '''
 
-df_agg = df_CityAvgPrice\
-    .writeStream \
-    .foreach(procss_df)\
-    .outputMode("complete") \
-    .start()
+    mysql_create_tbl_df = '''
+        create table if not exists yad2.df
+        (city_code numeric primary key ,
+        avg_SquareMeter numeric,
+        avg_price_per_SM numeric ,
+        count_city numeric);
+        '''
+
+    mysql_cursor = mysql_conn.cursor()
+    mysql_cursor.execute(mysql_create_tbl_events)
+    mysql_cursor.execute(mysql_create_tbl_df)
+    mysql_cursor.close()
+    pass
 
 
-spark.streams.awaitAnyTermination()
-'''df_yad2=df_kafka \
-    .writeStream \
-    .format("console") \
-    .start()\
-    .awaitTermination()'''
+def _init_kafka():
 
-#hdfs_query = df_yad2\
-#    .writeStream\
-#    .format("csv")\
-#    .option("path","hdfs://Cnt7-naya-cdh63:8020/user/hive/yad2")\
-#    .option("checkpointLocation","hdfs://Cnt7-naya-cdh63:8020/user/hive/yad2")\
-#    .outputMode("append")\
-#    .start()
+    spark_session = SparkSession \
+        .builder \
+        .appName("yad2") \
+        .getOrCreate()
+
+    bootstrap_server = 'Cnt7-naya-cdh63:9092'
+    topics = "yad2"
+
+    schema = StructType() \
+        .add('current_ts', StringType()) \
+        .add("record_id", IntegerType()) \
+        .add("ad_number", IntegerType()) \
+        .add("rooms", FloatType()) \
+        .add("floor", FloatType()) \
+        .add("SquareMeter", FloatType()) \
+        .add("price", FloatType()) \
+        .add("currency", StringType()) \
+        .add("city_code", IntegerType()) \
+        .add("city", StringType()) \
+        .add("street", StringType()) \
+        .add("AssetClassificationID_text", StringType()) \
+        .add("coordinates", StringType()) \
+        .add("date", StringType()) \
+        .add("date_added", StringType())
+
+    df_kafka = spark_session \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", bootstrap_server) \
+        .option("subscribe", topics) \
+        .load()
+
+    df_kafka = df_kafka.select(col("value").cast("string")) \
+        .select(from_json(col("value"), schema).alias("value")) \
+        .select("value.*")
+
+    df_kafka.printSchema()
+
+    df_kafka = df_kafka.withColumn("price_per_SM", df_kafka.price / df_kafka.SquareMeter)
+    df_kafka = df_kafka.withColumn("current_ts", current_timestamp().cast('string'))
+
+    df_CityAvgPrice = df_kafka \
+        .groupby("city_code") \
+        .agg(avg("SquareMeter").alias("avg_SquareMeter"), avg("price_per_SM").alias("avg_price_per_SM"),
+             count("record_id").alias("count_city"))
+
+    df_CityAvgPrice = df_CityAvgPrice.withColumn("current_ts", current_timestamp().cast('string'))
+
+    df_kafka \
+        .writeStream \
+        .foreach(procss_row) \
+        .outputMode("append") \
+        .start()
+
+    df_CityAvgPrice \
+        .writeStream \
+        .foreach(procss_df) \
+        .outputMode("complete") \
+        .start()
+
+    spark_session.streams.awaitAnyTermination()
 
 
-#hdfs_query.awaitTermination()
+def main():
+    _init_mysql()
+    # spark_session = _get_or_create_spark_session()
+    _init_kafka()
+
+
+if __name__ == '__main__':
+    main()
